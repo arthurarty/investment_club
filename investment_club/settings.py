2ebc4a0e9e286a2c4be83836f4a0a9013a051400
Arthur Nangai <arthur.nangai@gmail.com>
2025-08-25 20:56:00 +0200

    use the sample bootstrap sign in page

--- investment_club/settings.py
+++ investment_club/settings.py
@@ -123,6 +123,10 @@ USE_I18N = True
 
 USE_TZ = True
 
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
