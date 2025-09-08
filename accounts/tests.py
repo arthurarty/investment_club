bd4120fc16523fc013b731a8ab26aa0d897ce82b
Arthur Nangai <arthur.nangai@gmail.com>
2025-09-09 00:18:00 +0200

    Add test for the login logic

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,5 +1,9 @@
+from http import HTTPStatus
+
 from django.test import Client, TestCase
 from django.urls import reverse
+
+from accounts.models import CustomUser as User
 
 
 class LoginViewTestCase(TestCase):
@@ -8,5 +12,25 @@ class LoginViewTestCase(TestCase):
 
     def test_get_login_page(self):
         response = self.client.get(reverse("accounts:index"))
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, HTTPStatus.OK)
         self.assertTemplateUsed(response, "accounts/index.html")
+
+    def test_post_invalid_login(self):
+        response = self.client.post(
+            reverse("accounts:index"),
+            {"email": "jack@example.com", "password": "wrongpassword"},
+        )
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertTemplateUsed(response, "accounts/failed_login.html")
+
+    def test_post_valid_login(self):
+        email, password = "janedoe@example.com", "securepassword123"
+        User.objects.create_user(
+            email=email, password=password, first_name="Jane", last_name="Doe"
+        )
+        response = self.client.post(
+            reverse("accounts:index"), {"email": email, "password": password}
+        )
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertTemplateUsed(response, "accounts/successful_login.html")
+        self.assertContains(response, "Jane")
