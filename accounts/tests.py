9af21e5c50058ac8f991676155e7333f4bf4d24d
Arthur Nangai <arthur.nangai@gmail.com>
2025-09-09 00:05:55 +0200

    Add basic test for viewing landing page

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,12 @@
-from django.test import TestCase
+from django.test import Client, TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class LoginViewTestCase(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_get_login_page(self):
+        response = self.client.get(reverse("accounts:index"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "accounts/index.html")
