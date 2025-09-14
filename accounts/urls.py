ebab999f184cdb10bc3c4374f19c66f3f25bf055
Arthur Nangai <arthur.nangai@gmail.com>
2025-09-14 12:14:58 +0200

    Add logout logic

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from accounts.views import LoginView
+from accounts.views import LoginView, logout_view
 
 app_name = "accounts"
 
 urlpatterns = [
     path("", LoginView.as_view(), name="index"),
+    path("logout/", logout_view, name="logout"),
 ]
