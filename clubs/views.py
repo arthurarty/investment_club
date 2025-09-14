e06c5888085ec817dea19d17146ca69d1fb798f1
Arthur Nangai <arthur.nangai@gmail.com>
2025-09-14 12:11:49 +0200

    Use loginMixin

--- clubs/views.py
+++ clubs/views.py
@@ -1,8 +1,9 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
 from django.views import View
 
 
-class ClubsView(View):
+class ClubsView(LoginRequiredMixin, View):
     """
     View to display a list of investment clubs.
     """
