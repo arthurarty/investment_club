2028f4928bf3298be0a2fad4fc5d462185887cec
Arthur Nangai <arthur.nangai@gmail.com>
2025-09-14 11:36:28 +0200

    Add a relationship between a one to many relationship for club ownership

--- clubs/models.py
+++ clubs/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.db import models
 
 from common.models import BaseTimestampedModel
@@ -18,13 +19,19 @@ class Club(BaseTimestampedModel, models.Model):
     Model representing an investment club.
     """
 
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=350, unique=True)
     description = models.TextField()
     contact_phone = models.CharField(max_length=20, blank=True)
     contact_email = models.EmailField()
     status = models.CharField(
         max_length=20, choices=ClubStatus.choices, default=ClubStatus.ACTIVE
     )
+    created_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="created_clubs"
+    )
+    updated_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="updated_clubs"
+    )
 
     def __str__(self):
-        return str(self.name)
+        return f"{self.pk} - {self.name}"
