98b88f5c6a350ef7ca9af737993286facd26a224
Arthur Nangai <arthur.nangai@gmail.com>
2025-09-21 19:03:24 +0200

    [feat]- ClubMembership - Complete member lookup (#11)
    * Lock down user look up to club admins
    
    * Start testing the member view
    
    * update test to check redirect url
    
    * Add test for successful lookup
    
    * complete logic to add member to club
    
    * Use form to validate input
    
    * Complete logic to add member to a club and test it

--- clubs/urls.py
+++ clubs/urls.py
@@ -1,12 +1,15 @@
 from django.urls import path
 
 from clubs.views.club_views import ClubDetailView, ClubsListView
-from clubs.views.member_views import MemberLookUpView
+from clubs.views.member_views import AddMemberToClubView, MemberLookUpView
 
 app_name = "clubs"
 
 urlpatterns = [
     path("", ClubsListView.as_view(), name="index"),
     path("<int:club_id>/", ClubDetailView.as_view(), name="detail"),
-    path("member-lookup/", MemberLookUpView.as_view(), name="member-lookup"),
+    path(
+        "<int:club_id>/member-lookup/", MemberLookUpView.as_view(), name="member-lookup"
+    ),
+    path("<int:club_id>/add-member/", AddMemberToClubView.as_view(), name="add-member"),
 ]
--- clubs/views/member_views.py
+++ clubs/views/member_views.py
@@ -1,8 +1,10 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.views import View
 
+from accounts.models import CustomUser as User
 from clubs.forms.club_membership_form import MemberLookupForm
+from clubs.models import Club, ClubMember
 
 
 class MemberLookUpView(LoginRequiredMixin, View):
@@ -10,10 +12,19 @@ class MemberLookUpView(LoginRequiredMixin, View):
     View to look up and display member details.
     """
 
-    def post(self, request):
+    def post(self, request, club_id: int):
         """
         Handle POST requests to look up a member by email.
         """
+        club = Club.objects.get(id=club_id)
+        if not club:
+            # Todo: Add message to inform user that the club does not exist.
+            return redirect("clubs:index")
+        if not ClubMember.objects.filter(
+            club=club, user=request.user, is_admin=True
+        ).exists():
+            # Todo: Add message to inform user that they do not have permission to view this page.
+            return redirect("clubs:detail", club_id=club.id)
         form = MemberLookupForm(request.POST)
         if not form.is_valid():
             context = {
@@ -21,8 +32,46 @@ class MemberLookUpView(LoginRequiredMixin, View):
             }
             return render(request, "clubs/member_lookup.html", context)
         email = form.cleaned_data["email"]
+        try:
+            user = User.objects.get(email=email)
+            context = {
+                "look_up_form": form,
+                "member": user,
+                "email": email,
+                "club": club,
+            }
+            return render(request, "clubs/member_lookup.html", context)
+        except User.DoesNotExist:
+            form.add_error("email", "No user found with this email address.")
         context = {
             "look_up_form": form,
             "email": email,
         }
         return render(request, "clubs/member_lookup.html", context)
+
+
+class AddMemberToClubView(LoginRequiredMixin, View):
+    """
+    View to add a member to a club.
+    """
+
+    def get(self, request, club_id: int):
+        """
+        Handle POST requests to add a member to a club.
+        """
+        club = Club.objects.get(id=club_id)
+        if not club:
+            return redirect("clubs:index")
+        if not ClubMember.objects.filter(
+            club=club, user=request.user, is_admin=True
+        ).exists():
+            # Todo: Add message to inform user that they do not have permission to view this page.
+            return redirect("clubs:detail", club_id=club.id)
+        form = MemberLookupForm(request.GET)
+        if not form.is_valid():
+            return redirect("clubs:detail", club_id=club.id)
+        user = User.objects.filter(email=form.cleaned_data["email"]).first()
+        if not user:
+            return redirect("clubs:detail", club_id=club.id)
+        ClubMember.objects.get_or_create(club=club, user=user, is_admin=False)
+        return redirect("clubs:detail", club_id=club.id)
