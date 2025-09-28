8481656c9a4646999825cc107fe196c182ee6c1c
Arthur Nangai <arthur.nangai@gmail.com>
2025-09-28 19:10:25 +0200

    Add model to store transactions

--- clubs/admin.py
+++ clubs/admin.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
 
-from clubs.models import Club, ClubMember, FinancialYear, FinancialYearParticipant
+from clubs.models import (
+    Club,
+    ClubMember,
+    FinancialTransaction,
+    FinancialYear,
+    FinancialYearParticipant,
+)
 
 admin.site.register(Club)
 admin.site.register(ClubMember)
 admin.site.register(FinancialYear)
 admin.site.register(FinancialYearParticipant)
+admin.site.register(FinancialTransaction)
