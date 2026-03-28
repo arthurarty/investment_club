94b0baa4892966e08732e854341045f57b32a36e
Arthur Nangai <arthur.nangai@gmail.com>
2026-03-28 21:03:35 +0100

    Add an nginx config and docker container for it (#39)

---
+++ entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+set -e
+
+python manage.py migrate --noinput
+python manage.py collectstatic --noinput
+
+exec gunicorn investment_club.wsgi:application \
+    --bind 0.0.0.0:8000 \
+    --workers 3 \
+    --timeout 120 \
+    --access-logfile - \
+    --error-logfile -
