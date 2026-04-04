5461f964ab2773eefcf056e59fe74dd30c46bccb
Arthur Nangai <remands.shade_8q@icloud.com>
2026-04-04 18:28:11 +0200

    Add google recaptcha

--- accounts/login_form.py
+++ accounts/login_form.py
@@ -1,4 +1,6 @@
 from django import forms
+from django_recaptcha.fields import ReCaptchaField
+from django_recaptcha.widgets import ReCaptchaV3
 
 
 class LoginForm(forms.Form):
@@ -8,3 +10,4 @@ class LoginForm(forms.Form):
 
     email = forms.EmailField(label="Email", max_length=150)
     password = forms.CharField(label="Password", widget=forms.PasswordInput)
+    captcha = ReCaptchaField(widget=ReCaptchaV3)
