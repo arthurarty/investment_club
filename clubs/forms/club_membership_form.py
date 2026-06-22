cd304b18a988882943dfa869b043c5cc8504d15b
Arthur Nangai <17021137+arthurarty@users.noreply.github.com>
2026-06-22 19:18:34 +0200

    complete new membership form

--- clubs/forms/club_membership_form.py
+++ clubs/forms/club_membership_form.py
@@ -1,5 +1,7 @@
 from django import forms
 
+from accounts.form_validators.phone_number_validator import validate_phone_number
+from accounts.models import GenderChoices
 from clubs.models import MembershipCategory, MembershipStatus
 
 
@@ -22,6 +24,29 @@ class ClubMemberShipForm(forms.Form):
     Form to input the user's membership details
     """
 
+    email = forms.EmailField(widget=forms.EmailInput(attrs={"class": "form-control"}))
+    first_name = forms.CharField(
+        required=False, widget=forms.TextInput(attrs={"class": "form-control"})
+    )
+    last_name = forms.CharField(
+        required=False, widget=forms.TextInput(attrs={"class": "form-control"})
+    )
+    gender = forms.ChoiceField(
+        choices=GenderChoices.choices,
+        widget=forms.Select(attrs={"class": "form-control"}),
+    )
+    phone_number = forms.CharField(
+        validators=[validate_phone_number],
+        widget=forms.TextInput(
+            attrs={"class": "form-control", "placeholder": "+254712345678"}
+        ),
+    )
+    occupation = forms.CharField(
+        widget=forms.TextInput(attrs={"class": "form-control"})
+    )
+    physical_address = forms.CharField(
+        widget=forms.TextInput(attrs={"class": "form-control"})
+    )
     start_date = forms.DateField(
         widget=forms.DateInput(attrs={"type": "date", "class": "form-control"})
     )
