c007e3e2607cf42f61d84126a85a18b85d158efe
Arthur Nangai <17021137+arthurarty@users.noreply.github.com>
2026-07-12 08:40:20 +0300

    Add tests for flash message

--- clubs/tests/test_member_views.py
+++ clubs/tests/test_member_views.py
@@ -1,14 +1,16 @@
 from http import HTTPStatus
 
-from django.test import Client, TestCase
+from django.contrib.messages.storage.base import Message
+from django.test import Client
 from django.urls import reverse
 
 from accounts.models import CustomUser as User
 from accounts.models import GenderChoices
 from clubs.models import Club, ClubMembership, MembershipCategory, MembershipStatus
+from common.message_test_case import MsgTestCase
 
 
-class ClubMemberShipCreateViewTestCase(TestCase):
+class ClubMemberShipCreateViewTestCase(MsgTestCase):
     """
     Test case for the ClubMemberShipCreateView
     """
@@ -104,6 +106,13 @@ class ClubMemberShipCreateViewTestCase(TestCase):
                 club=self.investment_club, user=new_user
             ).exists()
         )
+        expected_messages = [
+            Message(
+                level=25,
+                message=f"{new_member_email} added to club: {self.investment_club.name}",
+            )
+        ]
+        self.assertMessages(response, expected_messages, ordered=True)
 
     def test_post_invalid_phone_number(self):
         """
